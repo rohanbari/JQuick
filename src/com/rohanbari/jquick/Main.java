306d62881f3bb4c49087d99bf47d3e734474f336
rohanbari <rohanbari4@gmail.com>
2020-11-26 09:59:09 +0530

    ConnectActivity class added

--- src/com/rohanbari/jquick/Main.java
+++ src/com/rohanbari/jquick/Main.java
@@ -1,8 +1,19 @@
 package com.rohanbari.jquick;
 
-public class Main {
+import static java.lang.System.out;
 
+public class Main {
     public static void main(String[] args) {
-	
+        int parser = ConnectActivity.connect();
+
+        if (parser == 100) {
+            out.println("This is a localhost address.");
+        } else if (parser == 1) {
+            out.println("This is a private address.");
+        } else {
+            out.println("Looks OK.");
+        }
+
+        out.println("Thanks for using this program!");
     }
 }
