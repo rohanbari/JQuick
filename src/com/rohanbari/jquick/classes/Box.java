3d9b208dc1d48d07d3f5a0b41b862237e98e12d3
Dipak Bari <dipakbari4@gmail.com>
2020-12-03 20:39:00 +0530

    Added Box class to manipulate Physical Box shaped object calculations

---
+++ src/com/rohanbari/jquick/classes/Box.java
@@ -0,0 +1,39 @@
+package com.rohanbari.jquick.classes;
+
+/**
+ * @author Dipak Bari
+ * @since 1.0
+ * @version 1.0
+ * @implNote Implement this class to use make of physical box properties and calculations.
+ */
+public class Box {
+    private double width;
+    private double height;
+    private double depth;
+
+    /**
+     * Empty constructor to use make implementation of setters
+     */
+    public Box() {
+    }
+
+    /**
+     * Construct box while creating object
+     * @param width Width of the Box in Specific Unit
+     * @param height Height of the Box in Specific Unit
+     * @param depth Depth of the Box in Specific Unit
+     */
+    public Box(double width, double height, double depth) {
+        this.width = width;
+        this.height = height;
+        this.depth = depth;
+    }
+
+    /**
+     * Method to get the volume of Box
+     * @return Returns the volume of Box
+     */
+    public double getVolume(){
+        return width * height * depth;
+    }
+}
