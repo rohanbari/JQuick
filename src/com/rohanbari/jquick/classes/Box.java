955ad2a6975c98a04e98ee252d748dc171e09f3c
Dipak Bari <dipakbari4@gmail.com>
2020-12-09 12:31:39 +0530

    Prints volume of the Box

--- src/com/rohanbari/jquick/classes/Box.java
+++ src/com/rohanbari/jquick/classes/Box.java
@@ -5,7 +5,9 @@ package com.rohanbari.jquick.classes;
  * @since 1.0
  * @version 1.0
  * @implNote Implement this class to use make of physical box properties and calculations.
+ * Use the <b>setters</b> and <b>getters</b> in case of empty constructor use.
  */
+
 public class Box {
     private double width;
     private double height;
@@ -35,5 +37,60 @@ public class Box {
      */
     public double getVolume(){
         return width * height * depth;
+    }
+
+    /**
+     * Prints volume of the Box
+     */
+    public void printVolume(){
+        System.out.println("Volume of the given Box: " + getVolume());
+    }
+
+    /**
+     * Sets the width of the Box
+     * @param width of the box
+     */
+    public void setWidth(double width) {
+        this.width = width;
+    }
+
+    /**
+     * Sets the height of the Box
+     * @param height of the Box
+     */
+    public void setHeight(double height) {
+        this.height = height;
+    }
+
+    /**
+     * Sets the depth of the Box
+     * @param depth of the Box
+     */
+    public void setDepth(double depth) {
+        this.depth = depth;
+    }
+
+    /**
+     * Get the width of the Box
+     * @return width of the box
+     */
+    public double getWidth() {
+        return width;
+    }
+
+    /**
+     * Get the height of the Box
+     * @return height of the Box
+     */
+    public double getHeight() {
+        return height;
+    }
+
+    /**
+     * Get the depth of the Box
+     * @return depth of the Box
+     */
+    public double getDepth() {
+        return depth;
     }
 }
