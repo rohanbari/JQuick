ef1a6ddbf054806321952c8c5b52926711a8f5cf
Dipak Bari <dipakbari4@gmail.com>
2020-12-09 12:32:26 +0530

    ProgramExecutor to execute simple java programs

---
+++ src/com/rohanbari/jquick/ProgramExecutor.java
@@ -0,0 +1,12 @@
+package com.rohanbari.jquick;
+
+import com.rohanbari.jquick.classes.Box;
+
+public class ProgramExecutor {
+    public static void main(String[] args) {
+        Box box = new Box(15.0, 23.0, 22.0);
+        double volume = box.getVolume();
+        System.out.println(volume);
+        box.printVolume();
+    }
+}
